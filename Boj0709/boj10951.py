7e9b3ac3a750c02efd0e4a7005e260a398ef3a8d
glossyyoon <mypju@naver.com>
2020-07-09 16:52:55 +0900

    commit

---
+++ Boj0709/boj10951.py
@@ -0,0 +1,11 @@
+try:
+    while(True):
+        num1, num2 = map(int, input().split())
+        print(num1+num2)
+except:
+    exit()
+    
+# try:
+#     실행할 코드
+# except:
+#     예외가 발생했을 때 처리하는 코드
