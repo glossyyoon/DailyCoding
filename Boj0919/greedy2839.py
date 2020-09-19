a2c16c1ac50a44105a9e48b53eb675c02614a79f
glossyyoon <mypju@naver.com>
2020-09-20 01:41:07 +0900

    greedy

--- Boj0919/greedy2839.py
+++ Boj0919/greedy2839.py
@@ -1,18 +1,27 @@
 import sys
 num = int(sys.stdin.readline())
-def five(num_in):
-    five = num//5
-    three = 0
-    if five%3 ==0 or five%5==0:
-        three = (num%5)//3
-        print(five+num)
-    else:
-        threes(num_in)
-    
-def threes(num_in):
-    three = num_in//3
-    if num_in%3!=0:
+box = 0
+while True:
+    if num%5==0:
+        box  = box + num//5
+        print(box)
+        break
+    num = num-3
+    box+=1
+    if num<0:
         print(-1)
-    else:
-        print(three)
-five(num)
+        break
+
+
+# inp = int(input())
+# Box = 0
+# while True:
+#     if (inp % 5) == 0:
+#         Box = Box + (inp//5)
+#         print(Box)
+#         break
+#     inp = inp-3
+#     Box += 1
+#     if inp < 0:
+#         print("-1")
+#         break
