814f5d52a9de26665a93af83e486111dd5a9abdd
glossyyoon <mypju@naver.com>
2020-10-09 15:36:40 +0900

    n과m

--- nm15650.py
+++ nm15650.py
@@ -2,18 +2,21 @@ import sys
 n, m = map(int, sys.stdin.readline().split(' '))
 check = [0 for _ in range(n+1)]
 result = [0 for _ in range(m)]
+num = [i for i in range(1, n+1)]
 def permutation(start):
     if start == m:
         for i in result:
             print(i, end = ' ')
         print()
     else:
-        # print("**start",start)
-        for i in range(1, n+1):
-            if check[i]==1:
+        for i in num:
+            if check[i] == 1 :
                 continue
-            check[i] = 1
-            result[start] = i
+            check[i]=1
+            result[start]=i
+            for j in range(i+1):
+                check[j] =1
             permutation(start+1)
-            check[i] = 0
+            for k in num:
+                check[k] = 0
 permutation(0)
--- nm15651.py
+++ nm15651.py
@@ -3,17 +3,12 @@ n, m = map(int, sys.stdin.readline().split(' '))
 check = [0 for _ in range(n+1)]
 result = [0 for _ in range(m)]
 def permutation(start):
-    if start == m:
+    if start==m:
         for i in result:
-            print(i, end = ' ')
+            print(i, end=' ')
         print()
     else:
-        # print("**start",start)
         for i in range(1, n+1):
-            if check[i]==1:
-                continue
-            check[i] = 1
-            result[start] = i
+            result[start]=i
             permutation(start+1)
-            check[i] = 0
 permutation(0)
