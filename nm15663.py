76234f7f9f35bcf1948bf63bcb732014d50a3f78
glossyyoon <mypju@naver.com>
2020-10-14 22:49:39 +0900

    n과m 중복

--- nm15663.py
+++ nm15663.py
@@ -7,20 +7,20 @@ result = [0 for _ in range(m)]
 num.sort()
 def permutation(start):
     if start==m:
-        for i in result:
+        results = list(set(result))
+        for i in results:
             print(i, end = ' ')
         print()
     else:
-        overlap = 0
+        # overlap = 0
         for i in range(1, n+1):
             if check[i]==1:
                 continue
-            if overlap!=num[i]:
-                result[start] = num[i]
-                check[i] = 1
-                result[start] = num[i]
-                overlap = num[i]
-                permutation(start+1)
-                check[i] = 0
+            # if overlap!=num[i]:
+            check[i] = 1
+            result[start] = num[i]
+            overlap = num[i]
+            permutation(start+1)
+            check[i] = 0
     
 permutation(0)
