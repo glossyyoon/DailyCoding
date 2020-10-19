33654a7d503f8402cf1e9e8d07eeb68097740ca4
glossyyoon <mypju@naver.com>
2020-10-19 15:27:26 +0900

    n과m 숫자 중복일때

--- nm15663.py
+++ nm15663.py
@@ -5,22 +5,23 @@ num.insert(0, 0)
 check = [0 for _ in range(len(num))]
 result = [0 for _ in range(m)]
 num.sort()
+arr = []
 def permutation(start):
     if start==m:
-        results = list(set(result))
-        for i in results:
+        arr.append(result)
+        for i in result:
             print(i, end = ' ')
         print()
     else:
-        # overlap = 0
+        overlap = 0
         for i in range(1, n+1):
             if check[i]==1:
                 continue
-            # if overlap!=num[i]:
-            check[i] = 1
-            result[start] = num[i]
-            overlap = num[i]
-            permutation(start+1)
-            check[i] = 0
+            if overlap!=num[i]:
+                check[i] = 1
+                result[start] = num[i]
+                overlap = num[i]
+                permutation(start+1)
+                check[i] = 0
     
 permutation(0)
