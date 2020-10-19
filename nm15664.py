5ec715c5274ca1f20365ed9fed9700f924800461
glossyyoon <mypju@naver.com>
2020-10-19 16:25:43 +0900

    nm10

---
+++ nm15664.py
@@ -0,0 +1,35 @@
+import sys
+n, m = map(int, sys.stdin.readline().split(' '))
+num = list(map(int, sys.stdin.readline().split(' ')))
+num.insert(0, 0)
+check = [0 for _ in range(len(num))]
+result = [0 for _ in range(m)]
+num.sort()
+arr = []
+def permutation(start):
+    if start==m:
+        arr.append(result)
+        for i in result:
+            print(i, end = ' ')
+        print()
+    else:
+        overlap = 0
+        for i in range(1, n+1):
+            if check[i]==1:
+                continue
+            if overlap!=num[i]:
+                if start>0:
+                    if num[i]>=result[start-1]:
+                        check[i] = 1
+                        result[start] = num[i]
+                        overlap = num[i]
+                        permutation(start+1)
+                        check[i] = 0
+                else:
+                    check[i] = 1
+                    result[start] = num[i]
+                    overlap = num[i]
+                    permutation(start+1)
+                    check[i] = 0
+
+permutation(0)
