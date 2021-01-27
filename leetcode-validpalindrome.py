aca85f5991e901ab8ce0014a6a894c3c7a188e9e
glossyyoon <mypju@naver.com>
2021-01-27 22:35:11 +0900

    isPalindrome

--- leetcode-validpalindrome.py
+++ leetcode-validpalindrome.py
@@ -2,7 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         strs = []
         for character in s:
-            if character.isalnum(): //영문자, 숫자를 판별하는 함수
+            if character.isalnum(): #isalnum(): 영문자, 숫자를 판별하는 함수
                 strs.append(character.lower())
         while len(strs)>1:
             if strs.pop(0) != strs.pop():
