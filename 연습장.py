e03b5d2cef30584448858f736c7096d6f7d927dc
glossyyoon <mypju@naver.com>
2021-02-09 17:30:40 +0900

    palindrome

--- 연습장.py
+++ 연습장.py
@@ -1,3 +1,6 @@
-a: str = "12345"
-for i in a:
-    print(i)
+import collections
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+anagram = collections.defaultdict(list)
+for word in strs:
+    anagram[''.join(sorted(word))].append(word)
+print(anagram.values())
