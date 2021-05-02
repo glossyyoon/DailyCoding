a9f2c80ec5c6cfbb7fa818f8da0bf87423997e9a
glossyyoon <mypju@naver.com>
2021-05-02 19:18:53 +0900

    최대공약수와 최소공배수

--- codePlus/basic2609.py
+++ codePlus/basic2609.py
@@ -2,18 +2,12 @@ a, b = map(int, input().split())
 
 
 def GCD(a, b):
-    for i in range(min(a, b)):
-        if a == 0 or b == 0:
-            # print(max(a, b))
-            return max(a, b)
-            break
-        else:
-            a, b = b, a % b
+    if b == 0:
+        return a
+    else:
+        return GCD(b, a % b)
 
 
-def LCM(a, b, rGCD):
-    return (rGCD * (a // rGCD) * (b // rGCD))
-
-
-print(GCD(a, b))
-print(LCM(a, b, GCD(a, b)))
+g = GCD(a, b)
+print(g)
+print(g * (a // g) * (b // g))
