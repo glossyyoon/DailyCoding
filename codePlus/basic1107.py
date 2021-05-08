536ff95271bc52ab8e108999e4f30ca185f9de49
glossyyoon <mypju@naver.com>
2021-05-08 13:51:33 +0900

    고장난 리모컨

--- codePlus/basic1107.py
+++ codePlus/basic1107.py
@@ -1,36 +1,32 @@
+channel = int(input())
 n = int(input())
-m = int(input())
-broken = [False] * 10
-if m > 0:
-    a = list(map(int, input().split()))
-else:
-    a = []
-for x in a:
-    broken[x] = True
+a = list(map(int, input().split()))
+button = [False] * 10
+for i in a:
+    button[i] = True  #고장난 버튼을 True로 바꿔줌.
 
 
 def possible(c):
     if c == 0:
-        if broken[0]:
-            return 0
-        else:
+        if button[c] == False:
             return 1
+        else:
+            return 0
     l = 0
     while c > 0:
-        if broken[c % 10]:
+        if button[c % 10]:
             return 0
-        l += 1
         c //= 10
+        l += 1
     return l
 
 
-ans = abs(n - 100)
-for i in range(0, 1000000 + 1):
+ans = abs(n - 100)  #정답의 초기값
+for i in range(1, 1000000 + 1):
     c = i
     l = possible(c)
     if l > 0:
-        press = abs(c - n)
+        press = abs(c - channel)
         if ans > l + press:
             ans = l + press
-
 print(ans)
