1f25f9ee832cd64083f6a18111278cbb56438f18
glossyyoon <mypju@naver.com>
2021-05-08 16:49:40 +0900

    카잉달력

--- codePlus/basic6064.py
+++ codePlus/basic6064.py
@@ -1,10 +1,12 @@
 t = int(input())
 for i in range(t):
     m, n, x, y = map(int, input().split())
+    x -= 1
+    y -= 1
     k = x
     while k < n * m:
         if k % n == y:
-            print(k)
+            print(k + 1)
             break
         k = k + m
     else:
