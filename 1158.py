89a885ee3370757281c52422933d5809ceab7851
glossyyoon <mypju@naver.com>
2021-07-07 15:17:58 +0900

    자료구조1

--- 1158.py
+++ 1158.py
@@ -1,1 +1,12 @@
-a
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+circle = [i for i in range(N)]
+result = []
+
+popNum = 0
+
+while circle:
+    popNum = (popNum + K - 1) % len(circle)
+    result.append(str(circle.pop(popNum) + 1))
+print("<%s>" % (", ").join(result))
