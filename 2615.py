1c3b74e481536fea1e60959cf4b4094b57daec75
glossyyoon <mypju@naver.com>
2021-07-15 23:52:32 +0900

    구현

--- 2615.py
+++ 2615.py
@@ -15,20 +15,20 @@ def o():
                     count = 1
                     while 0 <= nx < 19 and 0 <= ny < 19 and omok[i][j] == omok[nx][ny]:
                         count += 1
-                    if count == 5:
-                        if (
-                            0 <= nx + dx[k] < 19
-                            and 0 <= ny + dy[k]
-                            and omok[nx][ny] == omok[nx + dx[k]][ny + dy[k]]
-                        ):
-                            break
-                        if (
-                            0 <= nx + dx[k] < 19
-                            and 0 <= ny + dy[k]
-                            and omok[nx][ny] == omok[nx + dx[k]][ny + dy[k]]
-                        ):
-                            break
-                        return omok[i][j], i + 1, j + 1
+                        if count == 5:
+                            if (
+                                0 <= nx + dx[k] < 19
+                                and 0 <= ny + dy[k]
+                                and omok[nx][ny] == omok[nx + dx[k]][ny + dy[k]]
+                            ):
+                                break
+                            if (
+                                0 <= nx - dx[k] < 19
+                                and 0 <= ny - dy[k] < 19
+                                and omok[nx][ny] == omok[nx - dx[k]][ny - dy[k]]
+                            ):
+                                break
+                            return omok[i][j], i + 1, j + 1
                     nx = i + dx[k]
                     ny = j + dy[k]
 
