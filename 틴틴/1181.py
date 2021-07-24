3fe57b165b467f58bfb5d682758a4bf3abb8e4d8
glossyyoon <mypju@naver.com>
2021-07-25 00:12:41 +0900

    문자열

---
+++ 틴틴/1181.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+result = []
+for i in range(n):
+    w = str(input())
+    w_count = len(w)
+    result.append((w, w_count))
+result = list(set(result))
+result.sort(key=lambda word: (word[1], word[0]))
+for w in result:
+    print(w[0], end="")
