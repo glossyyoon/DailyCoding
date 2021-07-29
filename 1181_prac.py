cff608b697393cf940c858b9da9dc7b5cb27d2e4
glossyyoon <mypju@naver.com>
2021-07-29 18:03:54 +0900

    문자열

--- 1181_prac.py
+++ 1181_prac.py
@@ -2,10 +2,8 @@ n = int(input())
 r = []
 for i in range(n):
     s = input()
-    count = len(s)
     r.append((len(s), s))
 r = list(set(r))
 r.sort(key=lambda word: (word[0], word[1]))
-print(r)
 for i in range(len(r)):
     print(r[i][1])
--- 9342.py
+++ 9342.py
@@ -1,24 +1,66 @@
-t = int(input())
-must = "ABCDEF"
-for i in range(t):
-    dna = input()
-    count = 0
-    idx = 1
-    if not dna[0] in must:
-        idx = 0
+import sys
 
-    for j in range(idx, len(dna)):
-        if count == 0 and dna[j] == "A":
-            continue
-        if count == 0 and dna[j] == "F":
-            count += 1
-            continue
-        if count == 1 and dna[j] == "C":
-            count += 1
-            continue
-        if count == 2 and dna[j] in must and j == len(dna) - 1:
-            print("Infected!")
-            break
+t = int(sys.stdin.readline())
+
+
+def solution():
+    must = "ABCDEF"
+    dna = sys.stdin.readline().rstrip()
+    if len(dna) <= 2:
+        print("Good")
+        return
+    if dna[0] not in must:
+        print("Good")
+        return
+    if dna[-1] not in must:
+        print("Good")
+        return
+
+    if dna[0] != "A":
+        dna = dna[1:]
+
+    idx = 0
+    stage = 1
+    while idx < len(dna):
+        if stage == 1:
+            if dna[idx] != "A":
+                print("Good")
+                return
+            else:
+                stage = 2
+
+        elif stage == 2:
+            if dna[idx] == "A":
+                stage = 2
+            elif dna[idx] != "F":
+                print("Good")
+                return
+            else:
+                stage = 3
+
+        elif stage == 3:
+            if dna[idx] == "F":
+                stage = 3
+            elif dna[idx] != "C":
+                print("Good")
+                return
+            else:
+                stage = 4
+
+        elif stage == 4:
+            if dna[idx] == "C":
+                stage = 4
+            else:
+                stage = 5
+
         else:
             print("Good")
-            break
+            return
+
+        idx += 1
+    print("Infected!")
+
+
+while t > 0:
+    solution()
+    t -= 1
