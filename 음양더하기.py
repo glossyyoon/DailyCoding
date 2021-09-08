c9be0f26de148d5841304fd6a73c567a372e6a80
glossyyoon <mypju@naver.com>
2021-09-08 17:35:30 +0900

    level1

---
+++ 음양더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer += 0-absolutes[i]
+    return answer
