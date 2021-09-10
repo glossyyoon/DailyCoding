cc84fa11bf9106772f4ee368ed42f5db18fb838b
glossyyoon <mypju@naver.com>
2021-09-10 17:43:31 +0900

    review

--- 스터디/2668.py
+++ 스터디/2668.py
@@ -1,6 +1,7 @@
 import sys
 from collections import defaultdict
 
+
 def dfs(start, visited):
     visited.add(start)
     check[start] = 1
@@ -11,14 +12,15 @@ def dfs(start, visited):
             result.extend(list(visited))
             return
 
+
 n = int(sys.stdin.readline().rstrip())
 g = defaultdict(list)
-for i in range(1, n+1):
+for i in range(1, n + 1):
     v = int(sys.stdin.readline().strip())
     g[v].append(i)
-check = [ 0 for _ in range(n+1)]
+check = [0 for _ in range(n + 1)]
 result = []
-for i in range(n+1):
+for i in range(n + 1):
     if not check[i]:
         dfs(i, set([]))
 result.sort()
--- 스터디/3980.py
+++ 스터디/3980.py
@@ -2,10 +2,11 @@ import sys
 
 T = int(sys.stdin.readline().rstrip())
 
+
 def solution(idx, curr_sum):
     global max_sum
-    if idx==11:
-        if curr_sum>max_sum:
+    if idx == 11:
+        if curr_sum > max_sum:
             max_sum = curr_sum
             return
     for j in range(11):
@@ -13,15 +14,16 @@ def solution(idx, curr_sum):
             continue
         if player[idx][j] != 0:
             visit[j] = True
-            solution(idx+1, curr_sum + player[idx][j])
+            solution(idx + 1, curr_sum + player[idx][j])
             visit[j] = False
+
 
 for i in range(T):
     player = []
     for i in range(11):
         h = list(map(int, sys.stdin.readline().rstrip().split()))
         player.append(h)
-    visit = [False]*11
+    visit = [False] * 11
     max_sum = 0
-    solution(0,0)
+    solution(0, 0)
     print(max_sum)
