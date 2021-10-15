d420391b0f4989c7d59f9708830c62f4ac4c68e8
JiyunPark <mypju@naver.com>
2021-10-15 23:16:16 +0900

    [Boj] LCS

--- DP/2624.py
+++ DP/2624.py
@@ -8,7 +8,6 @@ L.sort()
 dp = [[0 for j in range(T + 1)] for i in range(k + 1)]
 for i in range(k + 1):
     dp[i][0] = 1
-
 for i in range(1, k + 1):
     for num in range(L[i][1] + 1):
         for j in range(T + 1):
