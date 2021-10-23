902a4676de9cc788f718e11d98c31e40b3f0cf2a
glossyyoon <mypju@naver.com>
2021-10-23 16:54:30 +0900

    [Programmers] 더 맵게

---
+++ 자료구조/42626.py
@@ -0,0 +1,16 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        if len(scoville) < 2 and scoville[0] < K:
+            return -1
+        mix = heapq.heappop(scoville) + heapq.heappop(scoville) * 2
+        heapq.heappush(scoville, mix)
+        answer += 1
+    return answer
+
+
+print(solution([1, 2, 3, 9, 10, 12], 7))
