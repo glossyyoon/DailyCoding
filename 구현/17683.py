4e7fcada5a6f91b21d2b33a1c5920c0d5df087c5
glossyyoon <mypju@naver.com>
2021-11-09 20:28:34 +0900

    [Programmers] 방금그곡

--- 구현/17683.py
+++ 구현/17683.py
@@ -1,26 +1,24 @@
-def solution(m, musicinfos):
-    start = 0
-    end = 1
-    name = 2
-    code = 3
-
-    for i in range(len(musicinfos)):
-        newCode = ""
-        newlist = musicinfos[i].split(",")
-        newlist[start] = int(newlist[start].replace(":", ""))
-        newlist[end] = int(newlist[end].replace(":", ""))
-        while newlist[end] - newlist[start] >= len(newCode) + 1:
-            newCode += newlist[code][: newlist[end] - newlist[start]]
-            if m == newCode:
-                return newlist[name]
-            elif m in newCode and newCode[newCode.index(m) + len(m)] != "#":
-                return newlist[name]
-        newCode = ""
-    return "(None)"
-
-
-print(
-    solution(
-        "CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]
+def change(melody):
+    melody = (
+        melody.replace("A#", "a")
+        .replace("C#", "c")
+        .replace("D#", "d")
+        .replace("F#", "f")
+        .replace("G#", "g")
     )
-)
+    return melody
+
+
+def solution(m, musicinfos):
+    m = change(m)
+    answer = ("(None)", None)
+    for info in musicinfos:
+        start, end, title, melody = info.split(",")
+        start_h, start_m, end_h, end_m = map(int, start.split(":") + end.split(":"))
+        time = 60 * (end_h - start_h) + (end_m - start_m)
+        melody = change(melody)
+        melody_played = (melody * time)[:time]
+        if m in melody_played:
+            if (answer[1] == None) or (time > answer[1]):
+                answer = (title, time)
+    return answer[0]
